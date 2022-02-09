2a48c42672e70f500bfa7c6aa963218d44d7ddbc
karam-zomlut <zomlutk@gmail.com>
2022-02-09 14:01:13 +0200

    add The Function Add Product (Pure)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 
+let arrOfProduct = [];
+let addProductToArray = (id, name, desc, price, img, category) => {
+    const product = {
+        id: id,
+        name: name,
+        desc: desc,
+        price: price,
+        img: img,
+        category: category
+    }
 
+    arrOfProduct.push(product);
+
+    return arrOfProduct;
+}
 
 
 
 module.exports = {
-    
+    addProductToArray
 }
--- test/functions.test.js
+++ test/functions.test.js
@@ -1,1 +1,22 @@
-const {} = require('../js/app.js');
+const {addProductToArray} = require('../js/app.js');
+
+describe("Karam", () => {
+    test("Should Return Product Object on Array", ()=> {
+        let actual = addProductToArray(0, "Men Watch", "Hello World", "15", "https://google.com", "Watch");
+        let expected = [
+            {
+                id: 0,
+                name: "Men Watch", 
+                desc: "Hello World",
+                price: "15",
+                img: "https://google.com",
+                category: "Watch"
+            }
+        ];
+    
+        expect(actual).toEqual(expected);
+    });
+
+
+
+});
