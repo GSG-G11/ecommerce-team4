515bf33d8421bcd1f046b688bbeddef6a90368ba
Maram Ismail <maramismail2015@gmail.com>
2022-02-09 13:13:29 +0200

    edit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+// let arrayOfProduct =[
+//     {
+//         id:0,
+//         name :'shirt',
+//         description:"this is a shirt",
+//         price :50,
+//         category:"clothes",
+    
+//     },
+//     {
+//         id:0,
+//         name :'shirt',
+//         description:"this is a shirt",
+//         price :50,
+//         category:"clothes",
+    
+//     },
+// ]
+// window.localStorage.setItem('user',JSON.stringify(arrayOfProduct));
+
