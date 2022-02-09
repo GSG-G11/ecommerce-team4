be468e4de73e81d3bc6022d58223bddda7200c53
karam-zomlut <zomlutk@gmail.com>
2022-02-09 20:50:18 +0200

    Edit Search Bar On Page Header Relates #45

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// Search Function
+let toggleSearch = document.querySelectorAll(".btn.toggle-search");
+let searchForm = document.querySelector("#search-form");
+
+toggleSearch.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        
+        let contain = searchForm.classList.contains('open');
+        contain ? searchForm.classList.remove("open") : searchForm.classList.add("open");
+        console.log("karam")
+    })
+})
+
+
+
 // Signup Form
 let sellerBtn = document.getElementById("seller-acc");
 let customerBtn = document.getElementById("customer-acc");
@@ -18,15 +34,3 @@ customerBtn.addEventListener("click", (e) => {
 });
 
 
-// Search Function
-let toggleSearch = document.querySelectorAll(".btn.toggle-search");
-let searchForm = document.querySelector("#search-form");
-
-toggleSearch.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        
-        let contain = searchForm.classList.contains('open');
-        contain ? searchForm.classList.remove("open") : searchForm.classList.add("open");
-    })
-})
