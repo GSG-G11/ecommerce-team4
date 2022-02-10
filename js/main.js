8ca801a425b93e39c6ffca8de798b4f5dd7ba475
karam-zomlut <zomlutk@gmail.com>
2022-02-10 10:38:31 +0200

    Fix Some Scripts Link Relates #50

--- js/main.js
+++ js/main.js
@@ -13,23 +13,6 @@ toggleSearch.forEach((btn) => {
 
 
 
-// Signup Form
-let sellerBtn = document.getElementById("seller-acc");
-let customerBtn = document.getElementById("customer-acc");
-let mainForm = document.querySelector(".form.singup-form");
 
-sellerBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let checkForm = mainForm.classList.contains("seller");
-
-    checkForm ? true : mainForm.classList.add("seller");
-});
-
-customerBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    let checkForm = mainForm.classList.contains("seller");
-
-    checkForm ? mainForm.classList.remove("seller") : true;
-});
 
 
